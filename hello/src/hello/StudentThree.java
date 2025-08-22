619f99dfc6e6de5cf9ffcc54420ae86ed2805645
Victor-Sarti <Sartivictor24@gmail.com>
2025-08-22 12:57:57 -0300

    radio Buttons

---
+++ hello/src/hello/StudentThree.java
@@ -0,0 +1,38 @@
+package hello;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class StudentThree {
+
+	private String firstName;
+	private String LastName;
+	private String favoriteLanguage;
+
+	public StudentThree() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+}
