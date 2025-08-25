45175b0850789898139593ba3bee01c79a07eabe
Victor-Sarti <Sartivictor24@gmail.com>
2025-08-25 09:30:01 -0300

    implementing custom validation

---
+++ validate-demo/src/com/vsarticode/jsf/validatedemo/StudentFour.java
@@ -0,0 +1,60 @@
+package com.vsarticode.jsf.validatedemo;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+@ManagedBean
+public class StudentFour {
+
+	private String firstName;
+	private String lastName;
+	private String courseCode;
+
+	public StudentFour() {
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public void validateTheCourseCode(FacesContext context, UIComponent component, Object value)
+			throws ValidatorException {
+
+		if (value == null) {
+			return;
+		}
+
+		String data = value.toString();
+
+		// course code must start with "STZ"
+		if (!data.startsWith("STZ")) {
+			FacesMessage message = new FacesMessage("Course code must start with STZ");
+			throw new ValidatorException(message);
+		}
+
+	}
+}
